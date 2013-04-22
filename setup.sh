e27aade9c1a94fa408616833845108f7c5f9d50a
Rick Button <me@rickybutton.com>
2013-04-22 04:27:09 -0400

    added vim diff

--- setup.sh
+++ setup.sh
@@ -5,7 +5,10 @@ FILES=( "gitconfig:$HOME/.gitconfig"
         "rick.zsh-theme:$HOME/.oh-my-zsh/themes/rick.zsh-theme"
         "tmux.conf:$HOME/.tmux.conf"
         "vimrc:$HOME/.vimrc"
-        "zshrc:$HOME/.zshrc" )
+        "zshrc:$HOME/.zshrc" 
+        "git_diff_wrapper:$HOME/bin/git_diff_wrapper")
+
+mkdir -p $HOME/bin
 
 for animal in "${FILES[@]}" ; do
   KEY=${animal%%:*}
