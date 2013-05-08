6be76cf52f311ef9f586a04b497e19935a208ac2
Rick Button <me@rickybutton.com>
2013-05-08 19:59:14 -0400

    colors

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+set -e
+
+echo -e -n "\033[1;37mRick Button's dotfiles\n\n"
 
 FILES=( "gitconfig:$HOME/.gitconfig"
         "gitignore_global:$HOME/.gitignore_global"
@@ -10,11 +13,24 @@ FILES=( "gitconfig:$HOME/.gitconfig"
         "vim:$HOME/.vim"
         "upload.py:$HOME/bin/upload.py")
 
-mkdir -p $HOME/bin
+FOLDERS=( "$HOME/bin" )
 
-for animal in "${FILES[@]}" ; do
-  KEY=${animal%%:*}
-  VALUE=${animal#*:}
+echo -e -n "\033[1;36mCreating folders...\n"
+for d in "${FOLDERS[@]}" ; do
+  VALUE=${d}
+  echo -e -n "\033[1;32m$VALUE\n"
+  rm -rf $VALUE
+  mkdir -p $VALUE
+done
+echo -e -n "\n"
+echo -e -n "\033[1;36mSymlinking files...\n"
+for f in "${FILES[@]}" ; do
+  KEY=${f%%:*}
+  VALUE=${f#*:}
+  echo -e -n "\033[1;32m$PWD/$KEY -> $VALUE\n"
   rm -rf $VALUE
   ln -s $PWD/$KEY $VALUE
 done
+
+echo -e -n "\n"
+echo -e -n "\033[1;37mDone.\n"
